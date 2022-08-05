c40a98782792fb361115a81826430f8a70d5f26b
Paulo Machado <pffmachado@yahoo.com>
2022-08-05 09:42:51 +0100

    Fix typo
    Signed-off-by: Paulo Machado <pffmachado@yahoo.com>

--- src/tclhdl-libero.tcl
+++ src/tclhdl-libero.tcl
@@ -167,9 +167,9 @@ proc ::tclhdl::libero::open_project {args} {
     #-- multiple times, then we set twice became the vault always different.
     log::log debug "libero::open_project: Force Vault at $::env(HOME)/.microsemi/libero/vault"
     file mkdir "$current_dir/vault"
-    change_vault_location -location "$current_dir/vault
+    change_vault_location -location "$current_dir/vault"
     file mkdir "$::env(HOME)/.microsemi/libero/vault"
-    change_vault_location -location "/.microsemi/libero/vault
+    change_vault_location -location "/.microsemi/libero/vault"
 
     #if { [catch {exec >&@stdout change_vault_location -location "$current_dir/vault"}] } {
     #    log::log debug "libero::open_project: Force Vault at $current_dir/vault"
